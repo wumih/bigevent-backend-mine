de86c994680b5d1ba2fad81cc5a9686c844ab5a8
wumih <120448608+wumih@users.noreply.github.com>
2025-11-11 22:19:35 +0800

    feat: initial commit for API backend

---
+++ app.js
@@ -0,0 +1,17 @@
+// 导入 express 模块
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
+// 导入 cors 中间件
+const cors = require('cors')
+// 将 cors 注册为全局中间件
+app.use(cors())
+// 导入并注册用户路由模块
+const userRouter = require('./router/user')
+app.use('/api', userRouter)
+// write your code here...
+app.use(express.urlencoded({ extended: false }))
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(3007, function () {
+  console.log('api server running at http://127.0.0.1:3007')
+})
---
+++ router/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+// 创建路由对象
+const router = express.Router()
+
+// 注册新用户
+router.post('/reguser', (req, res) => {
+  res.send('reguser OK')
+})
+
+// 登录
+router.post('/login', (req, res) => {
+  res.send('login OK')
+})
+
+// 将路由对象共享出去
+module.exports = router
